1181bed813f8ba4434fd28448a9614ea19a98f9f
kedarcaja <kedarcaja@gmail.com>
2017-06-18 13:16:01 +0200

    Uspořádání

---
+++ Fate Of Hero/Assets/Script/dialog.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class dialog : MonoBehaviour {
+
+	public float letterPause = 0.2f;
+
+	public string message;
+	Text textComp;
+
+	// Use this for initialization
+	void Start () {
+		textComp = GetComponent<Text>();
+		message = textComp.text;
+		textComp.text = "AHOJ";
+		StartCoroutine(TypeText ());
+	}
+
+	IEnumerator TypeText () {
+		foreach (char letter in message.ToCharArray()) {
+			textComp.text += letter;
+
+				
+			yield return 0;
+			yield return new WaitForSeconds (letterPause);
+		}
+	}
+}

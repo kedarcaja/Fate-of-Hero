ac2b1f32317f637020e553c783deeeff68276a80
kedarcaja <kedarcaja@gmail.com>
2017-07-16 23:47:12 +0200

    Menu

---
+++ Fate Of Hero/Assets/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class LevelManager : MonoBehaviour {
+	public Transform mainMenu, optionsMenu;
+
+	public void Loadlevel()
+    {
+        SceneManager.LoadScene("UvodniLokace");	
+	}
+	public void QuitGame(){
+		Application.Quit();
+	}
+	public void OptionsMenu(bool clicked){
+		if (clicked == true){
+			optionsMenu.gameObject.SetActive(clicked);
+			mainMenu.gameObject.SetActive(false);
+		} else {
+			optionsMenu.gameObject.SetActive(clicked);
+			mainMenu.gameObject.SetActive(true);
+		}
+	}
+
+
+}

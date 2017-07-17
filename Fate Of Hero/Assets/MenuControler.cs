12ad55bfad83be9f81d230881b29d329d096fe46
kedarcaja <kedarcaja@gmail.com>
2017-07-17 22:20:54 +0200

    Menu

---
+++ Fate Of Hero/Assets/MenuControler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControler : MonoBehaviour {
+
+    public void Loadlevel()
+    {
+        SceneManager.LoadScene("UvodniLokace");
+        Debug.Log("As you wish! :)");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("As you wish! :)");
+        Application.Quit();
+    }
+}

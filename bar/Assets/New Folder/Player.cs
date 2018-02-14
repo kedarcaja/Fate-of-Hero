3b4c50b853f522722d3ba255784c4a43b840da9f
kedarcaja <kedarcaja@gmail.com>
2018-02-14 13:16:36 +0100

    Lávka

---
+++ bar/Assets/New Folder/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    #region Varibles
+    [SerializeField]
+    private stats health;
+    #endregion
+
+    #region Unity methods
+
+    private void Awake()
+    {
+        health.Initialize();
+    }
+
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            health.CurrentVal -= 10;
+            
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            health.CurrentVal += 10;
+        }
+
+    }
+    #endregion
+}
---
+++ bar/Assets/New Folder/stats.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class stats
+{
+    private BS bar;
+    #region Varibles
+    [SerializeField]
+    private float maxVal;
+
+    [SerializeField]
+    private float currentVal;
+
+    public float CurrentVal
+    {
+        get
+        {
+            return currentVal;
+        }
+
+        set
+        {
+            currentVal = value;
+        }
+    }
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+
+        set
+        {
+            maxVal = value;
+        }
+    }
+
+
+
+    #endregion
+
+    #region Unity methods
+    public void Initialize()
+    {
+        this.MaxVal = maxVal;
+        this.CurrentVal = currentVal;
+
+    }
+    #endregion
+}

5e543d02f8c7e4ac1213cc852892caea090319c7
kedarcaja <kedarcaja@gmail.com>
2018-03-04 15:45:09 +0100

    Wall update

---
+++ Fate Of Hero/Assets/WallFaded.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WallFaded : MonoBehaviour {
+    #region Varibles
+    [SerializeField]
+    private GameObject wall;
+
+    private Color def;
+    private Color fade;
+   #endregion
+   
+   #region Unity methods
+  
+	void Start () {
+        def = wall.GetComponent<SpriteRenderer>().color;
+        fade = def;
+        fade.a = 0.7f;
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            wall.GetComponent<SpriteRenderer>().color = fade;
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            wall.GetComponent<SpriteRenderer>().color = def;
+        }
+    }
+    #endregion
+}

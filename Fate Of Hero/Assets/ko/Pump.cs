3973d889b8851baf52a23d787b0ce0c19eb704fd
vitekd55 <vicadvorak@seznam.cz>
2018-04-25 23:19:39 +0200

    kovárna základ

---
+++ Fate Of Hero/Assets/ko/Pump.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Pump : MonoBehaviour {
+
+    private Slider PumpSlider;
+
+    private bool canBoost = true;
+    [SerializeField]
+    private RectTransform BellowRectTransform;
+    private void Awake()
+    {
+      
+        PumpSlider = GetComponent<Slider>();
+      
+        PumpSlider.maxValue = 135;
+        PumpSlider.value = PumpSlider.maxValue;
+        PumpSlider.minValue = 70;
+
+
+
+    }
+
+    private void Update()
+    {
+
+
+        BellowRectTransform.sizeDelta = new Vector2(207, PumpSlider.value);
+       if(PumpSlider.value== PumpSlider.maxValue&&canBoost)
+            SpeedBoost();
+          
+           
+
+          
+        
+        if (PumpSlider.value == PumpSlider.minValue)
+            canBoost = true;
+
+
+    }
+
+
+
+    private void SpeedBoost()
+    {
+        canBoost = false;
+
+        TemperatureMeter.speed += 5;
+    }
+}

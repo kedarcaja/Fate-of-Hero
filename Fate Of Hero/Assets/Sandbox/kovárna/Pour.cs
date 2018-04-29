2f7fb1dfe8a6f4d04e585de58566185bbbedfd9d
vitekd55 <vicadvorak@seznam.cz>
2018-04-29 21:37:59 +0200

    oprava setrvačnosti

--- Fate Of Hero/Assets/Sandbox/kovárna/Pour.cs
+++ Fate Of Hero/Assets/Sandbox/kovárna/Pour.cs
@@ -8,7 +8,7 @@ public class Pour : MonoBehaviour {
     private bool SetStart = false;
     private float CurrentValue,returnValue;
     [SerializeField]
-    private float speed,max, min;
+    private float speedRotation,max, min,speed;
     private void Start()
     {
         Puller = FindObjectOfType<Slider>();
@@ -19,11 +19,16 @@ public class Pour : MonoBehaviour {
     public void TurnVat() {
         if (Puller.value >CurrentValue&& transform.eulerAngles.x > max)
         {
-            transform.Rotate(new Vector3(0, speed));
+            transform.Rotate(new Vector3(0, speedRotation));
            
 
         }
-     
+        if (Puller.value < CurrentValue)
+        {
+
+            Puller.interactable = false;
+            SetStart = true;
+        }
         CurrentValue = Puller.value;
         
 	}
@@ -34,14 +39,17 @@ public class Pour : MonoBehaviour {
 
         if (SetStart)
         {
-            Puller.value = Mathf.Lerp(Puller.value, Puller.minValue, Time.deltaTime);
-            returnValue = Mathf.Lerp(transform.eulerAngles.x, min, Time.deltaTime);
+            Puller.value = Mathf.Lerp(Puller.value, Puller.minValue, Time.deltaTime*speed);
+            returnValue = Mathf.Lerp(transform.eulerAngles.x, min, Time.deltaTime*speed);
             transform.eulerAngles = new Vector3(returnValue, transform.eulerAngles.y, transform.eulerAngles.z);
 
         }
 
         if (Input.GetMouseButtonDown(0))
+        {
             SetStart = false;
+            Puller.interactable = true;
+        }
         if (Input.GetMouseButtonUp(0))
             SetStart = true;
     }

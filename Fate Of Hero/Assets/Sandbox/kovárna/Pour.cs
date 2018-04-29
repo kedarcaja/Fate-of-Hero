9ae70d1c5f6011e1905002921461dc8e3a3f66cd
vitekd55 <vicadvorak@seznam.cz>
2018-04-29 18:08:56 +0200

    náklon tažením

---
+++ Fate Of Hero/Assets/Sandbox/kovárna/Pour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pour : MonoBehaviour {
+    private Slider Puller;
+    private float CurrentValue;
+    [SerializeField]
+    private float speed;
+    private void Start()
+    {
+        Puller = FindObjectOfType<Slider>();
+    }
+
+    public void TurnVat() {
+        if (Puller.value > CurrentValue)
+        {
+            transform.Rotate(new Vector3(0, speed));
+            print("+");
+
+        }
+        if (Puller.value < CurrentValue)
+        {
+            transform.Rotate(new Vector3(0, -speed));
+            print("-");
+        }
+
+        CurrentValue = Puller.value;
+        
+	}
+    private void Update()
+    {
+       
+       
+
+        
+           
+     
+
+    }
+}

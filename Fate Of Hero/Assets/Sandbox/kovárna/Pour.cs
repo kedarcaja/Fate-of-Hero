b041c3f82500285fa74b6dfcfad0588ad30601e2
vitekd55 <vicadvorak@seznam.cz>
2018-04-29 20:50:45 +0200

    setrvačnost

--- Fate Of Hero/Assets/Sandbox/kovárna/Pour.cs
+++ Fate Of Hero/Assets/Sandbox/kovárna/Pour.cs
@@ -5,38 +5,45 @@ using UnityEngine.UI;
 
 public class Pour : MonoBehaviour {
     private Slider Puller;
-    private float CurrentValue;
+    private bool SetStart = false;
+    private float CurrentValue,returnValue;
     [SerializeField]
-    private float speed;
+    private float speed,max, min;
     private void Start()
     {
         Puller = FindObjectOfType<Slider>();
+        max = 280;
+        min = 359;
     }
 
     public void TurnVat() {
-        if (Puller.value > CurrentValue)
+        if (Puller.value >CurrentValue&& transform.eulerAngles.x > max)
         {
             transform.Rotate(new Vector3(0, speed));
-            print("+");
+           
 
         }
-        if (Puller.value < CurrentValue)
-        {
-            transform.Rotate(new Vector3(0, -speed));
-            print("-");
-        }
-
+     
         CurrentValue = Puller.value;
         
 	}
     private void Update()
     {
-       
-       
 
-        
-           
-     
 
+
+        if (SetStart)
+        {
+            Puller.value = Mathf.Lerp(Puller.value, Puller.minValue, Time.deltaTime);
+            returnValue = Mathf.Lerp(transform.eulerAngles.x, min, Time.deltaTime);
+            transform.eulerAngles = new Vector3(returnValue, transform.eulerAngles.y, transform.eulerAngles.z);
+
+        }
+
+        if (Input.GetMouseButtonDown(0))
+            SetStart = false;
+        if (Input.GetMouseButtonUp(0))
+            SetStart = true;
     }
+    
 }

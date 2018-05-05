ee1886be769167aa4c95d8ea9df6e4976290070a
kedarcaja <kedarcaja@gmail.com>
2018-05-05 16:34:48 +0200

    Achievment systém
    aplikování kové komponenty
    základ pro přidání ocenění za jenlotlové úspěchy:
    základní
    skupinový
    víceprvkové

---
+++ Fate Of Hero/Assets/Sandbox/Kedarcaja/Achievment/Script/AchvementButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchvementButton : MonoBehaviour {
+    #region Variables
+    public GameObject achvementList;
+
+    public Sprite neutral, highlight;
+
+    private Image sprite;
+    #endregion
+    #region Metods
+    private void Awake()
+    {
+        sprite = GetComponent<Image>();
+    }
+    public void Click()
+    {
+        if (sprite.sprite == neutral)
+        {
+            sprite.sprite = highlight;
+            achvementList.SetActive(true);
+        }
+        else
+        {
+            sprite.sprite = neutral;
+            achvementList.SetActive(false);
+        }
+    }
+
+   
+     #endregion
+}
---
+++ Fate Of Hero/Assets/Sandbox/Kedarcaja/Achievment/Script/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestScript : MonoBehaviour {
+    public string achievmentName;
+	
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+    private void OnMouseDown()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject(-1))
+        {
+            AchivementManager.Instance.EarnAchievment(achievmentName);
+        }
+       
+    }
+}

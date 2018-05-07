96927661bcc0d8246ab8ac706aca9527991b71f2
vitekd55 <vicadvorak@seznam.cz>
2018-05-07 22:00:34 +0200

    pozice palice

---
+++ Fate Of Hero/Assets/Sandbox/kovárna/HItPosition.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HItPosition : MonoBehaviour {
+
+    private float MoveSpeed,StartPosition,EndPosition,YPositionDown,YPositionUP;
+	void Start () {
+        StartPosition = 370;
+        EndPosition = -749;
+        YPositionDown = -72;
+        YPositionUP = 34;
+        MoveSpeed = 10;
+        transform.localPosition = new Vector3(StartPosition,YPositionDown);
+
+	}
+	
+
+	void Update () {
+
+
+        transform.localPosition = new Vector3(transform.localPosition.x + MoveSpeed, transform.localPosition.y);
+		if(transform.localPosition.x < EndPosition || transform.localPosition.x > StartPosition)
+        {
+
+
+            MoveSpeed *= -1;
+        }
+
+        if (transform.localPosition.x <= EndPosition)
+        {
+
+
+            transform.localPosition = new Vector3(transform.localPosition.x, YPositionUP);
+        }
+        else if(transform.localPosition.x>=StartPosition)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, YPositionDown);
+
+        }
+    }
+}

fce69342d690c7393291cd8277516675a36e33ac
vitekd55 <vicadvorak@seznam.cz>
2018-05-08 13:09:59 +0200

    power bar

---
+++ Fate Of Hero/Assets/Sandbox/kovárna/Scripts/kování/Blade.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour {
+
+public static float AllHitPower;
+    public static int HitValue;
+	void Start () {
+        HitValue = 10;
+		
+	}
+	
+
+	void Update () {
+        print("Current Power: "+AllHitPower);
+        print("value of hits: " + HitValue);
+
+    }
+}
--- Fate Of Hero/Assets/Sandbox/kovárna/Scripts/kování/HitPower.cs
+++ Fate Of Hero/Assets/Sandbox/kovárna/Scripts/kování/HitPower.cs
@@ -6,32 +6,44 @@ public class HitPower : MonoBehaviour {
 
     private Slider PowerSlider;
     private float PowerValue,PowerSpeed;
-	void Start () {
+    public bool isChoosingPower = false;
+    private HItPosition PositionHit;
+        void Start () {
         PowerSlider = GetComponent<Slider>();
         PowerSlider.interactable = false;
         PowerSpeed = 0.01f;
+        PositionHit = FindObjectOfType<HItPosition>();
 	}
 
     // Update is called once per frame
     void Update()
     {
-        PowerSlider.value += PowerSpeed;
-        if (PowerSlider.value == PowerSlider.minValue)
+        if (isChoosingPower)
+            PowerSlider.value += PowerSpeed;
+            if (PowerSlider.value == PowerSlider.minValue)
+            {
+                PowerSpeed -= 0.001f;
+                PowerSpeed *= -1;
+
+            }
+            if (PowerSlider.value == PowerSlider.maxValue)
+            {
+                PowerSpeed += 0.001f;
+
+                PowerSpeed *= -1;
+
+            }
+
+        if (Input.GetKeyDown(KeyCode.S)&&Blade.HitValue>0)
+
         {
-            PowerSpeed -= 0.001f;
-            PowerSpeed *= -1;
+            PowerValue = PowerSlider.value;
+            isChoosingPower = false;
+            PositionHit.isChoosingPosition = true;
+           
+            Blade.AllHitPower += PowerValue;
+            Blade.HitValue--;
 
-        }
-        if (PowerSlider.value == PowerSlider.maxValue)
-        {
-            PowerSpeed += 0.001f;
-
-            PowerSpeed *= -1;
-
-        }
-        if (Input.GetMouseButtonDown(0))
-        { PowerValue = PowerSlider.value;
-            PowerSpeed = 0;
         }
     }
 }

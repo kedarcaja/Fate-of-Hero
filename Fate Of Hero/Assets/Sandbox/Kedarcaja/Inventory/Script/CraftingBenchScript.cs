7d7298b6db31308ffab9f84e5ef09d52fe1b78cf
kedarcaja <kedarcaja@gmail.com>
2018-05-08 13:32:22 +0200

    Inventory

---
+++ Fate Of Hero/Assets/Sandbox/Kedarcaja/Inventory/Script/CraftingBenchScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingBenchScript : MonoBehaviour {
+
+    #region Variables
+    public Inventory craftingBench;
+   #endregion
+     
+   #region Unity Metod
+   
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+  #endregion  
+}

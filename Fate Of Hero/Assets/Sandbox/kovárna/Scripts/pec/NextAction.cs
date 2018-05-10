c5f8b7ea04f6f28ebfb01ed9cca2bc9db4a0fc88
vitekd55 <vicadvorak@seznam.cz>
2018-05-10 19:06:04 +0200

    menu

--- Fate Of Hero/Assets/Sandbox/kovárna/Scripts/pec/NextAction.cs
+++ Fate Of Hero/Assets/Sandbox/kovárna/Scripts/pec/NextAction.cs
@@ -9,4 +9,16 @@ public class NextAction : MonoBehaviour {
         ToActive.SetActive(true);
         gameObject.SetActive(false);
     }
+    public void ChooseAction(GameObject Active)
+    {
+        for(int i = 0; Active.transform.parent.childCount>i;i++)
+        {
+
+            if(Active.transform.parent.GetChild(i)!= Active)
+            Active.transform.parent.GetChild(i).gameObject.SetActive(false);
+
+        }
+        Active.SetActive(true);
+
+    }
 }

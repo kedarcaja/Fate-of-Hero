1d8840b73fa6f3ae59b78074c5468bef64dce939
kedarcaja <kedarcaja@gmail.com>
2018-05-19 22:06:55 +0200

    Free look cam

--- Fate Of Hero/Assets/Standard Assets/Cameras/Scripts/FreeLookCam.cs
+++ Fate Of Hero/Assets/Standard Assets/Cameras/Scripts/FreeLookCam.cs
@@ -1,10 +1,10 @@
 using System;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
+//using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Cameras
 {
-    public class FreeLookCam : PivotBasedCameraRig
+    public class FreeLookCam //: PivotBasedCameraRig
     {
         // This script is designed to be placed on the root object of a camera rig,
         // comprising 3 gameobjects, each parented to the next:
@@ -28,17 +28,17 @@ namespace UnityStandardAssets.Cameras
 		private Quaternion m_PivotTargetRot;
 		private Quaternion m_TransformTargetRot;
 
-        protected override void Awake()
-        {
-            base.Awake();
-            // Lock or unlock the cursor.
-            Cursor.lockState = m_LockCursor ? CursorLockMode.Locked : CursorLockMode.None;
-            Cursor.visible = !m_LockCursor;
-			m_PivotEulers = m_Pivot.rotation.eulerAngles;
+   //     protected override void Awake()
+   //     {
+   //         base.Awake();
+   //         // Lock or unlock the cursor.
+   //         Cursor.lockState = m_LockCursor ? CursorLockMode.Locked : CursorLockMode.None;
+   //         Cursor.visible = !m_LockCursor;
+			//m_PivotEulers = m_Pivot.rotation.eulerAngles;
 
-	        m_PivotTargetRot = m_Pivot.transform.localRotation;
-			m_TransformTargetRot = transform.localRotation;
-        }
+	  //      m_PivotTargetRot = m_Pivot.transform.localRotation;
+			//m_TransformTargetRot = transform.localRotation;
+   //     }
 
 
         protected void Update()
@@ -59,12 +59,12 @@ namespace UnityStandardAssets.Cameras
         }
 
 
-        protected override void FollowTarget(float deltaTime)
-        {
-            if (m_Target == null) return;
-            // Move the rig towards target position.
-            transform.position = Vector3.Lerp(transform.position, m_Target.position, deltaTime*m_MoveSpeed);
-        }
+        //protected override void FollowTarget(float deltaTime)
+        //{
+        //    if (m_Target == null) return;
+        //    // Move the rig towards target position.
+        //    transform.position = Vector3.Lerp(transform.position, m_Target.position, deltaTime*m_MoveSpeed);
+        //}
 
 
         private void HandleRotationMovement()
@@ -73,11 +73,11 @@ namespace UnityStandardAssets.Cameras
 			return;
 
             // Read the user input
-            var x = CrossPlatformInputManager.GetAxis("Mouse X");
-            var y = CrossPlatformInputManager.GetAxis("Mouse Y");
+            //var x = CrossPlatformInputManager.GetAxis("Mouse X");
+            //var y = CrossPlatformInputManager.GetAxis("Mouse Y");
 
             // Adjust the look angle by an amount proportional to the turn speed and horizontal input.
-            m_LookAngle += x*m_TurnSpeed;
+            //m_LookAngle += x*m_TurnSpeed;
 
             // Rotate the rig (the root object) around Y axis only:
             m_TransformTargetRot = Quaternion.Euler(0f, m_LookAngle, 0f);
@@ -87,12 +87,12 @@ namespace UnityStandardAssets.Cameras
                 // For tilt input, we need to behave differently depending on whether we're using mouse or touch input:
                 // on mobile, vertical input is directly mapped to tilt value, so it springs back automatically when the look input is released
                 // we have to test whether above or below zero because we want to auto-return to zero even if min and max are not symmetrical.
-                m_TiltAngle = y > 0 ? Mathf.Lerp(0, -m_TiltMin, y) : Mathf.Lerp(0, m_TiltMax, -y);
+               // m_TiltAngle = y > 0 ? Mathf.Lerp(0, -m_TiltMin, y) : Mathf.Lerp(0, m_TiltMax, -y);
             }
             else
             {
                 // on platforms with a mouse, we adjust the current angle based on Y mouse input and turn speed
-                m_TiltAngle -= y*m_TurnSpeed;
+                //m_TiltAngle -= y*m_TurnSpeed;
                 // and make sure the new value is within the tilt range
                 m_TiltAngle = Mathf.Clamp(m_TiltAngle, -m_TiltMin, m_TiltMax);
             }
@@ -102,13 +102,13 @@ namespace UnityStandardAssets.Cameras
 
 			if (m_TurnSmoothing > 0)
 			{
-				m_Pivot.localRotation = Quaternion.Slerp(m_Pivot.localRotation, m_PivotTargetRot, m_TurnSmoothing * Time.deltaTime);
-				transform.localRotation = Quaternion.Slerp(transform.localRotation, m_TransformTargetRot, m_TurnSmoothing * Time.deltaTime);
+				//m_Pivot.localRotation = Quaternion.Slerp(m_Pivot.localRotation, m_PivotTargetRot, m_TurnSmoothing * Time.deltaTime);
+				//transform.localRotation = Quaternion.Slerp(transform.localRotation, m_TransformTargetRot, m_TurnSmoothing * Time.deltaTime);
 			}
 			else
 			{
-				m_Pivot.localRotation = m_PivotTargetRot;
-				transform.localRotation = m_TransformTargetRot;
+				//m_Pivot.localRotation = m_PivotTargetRot;
+				//transform.localRotation = m_TransformTargetRot;
 			}
         }
     }

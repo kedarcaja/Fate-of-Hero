cfeff8b039bf5c8ebede3505529076e4eb395cf9
vitekd55 <vicadvorak@seznam.cz>
2018-05-20 21:20:56 +0200

    meč

---
+++ Fate Of Hero/Assets/Sandbox/kovárna/Scripts/broušení/Sword.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+
+    private KeyCode forward = KeyCode.UpArrow;
+    private KeyCode backward = KeyCode.DownArrow;
+    private KeyCode left = KeyCode.LeftArrow;
+    private KeyCode right = KeyCode.RightArrow;
+    private float RotateAngelMax = 285;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+       if(Input.GetKey(forward)&& transform.eulerAngles.x>RotateAngelMax)
+            transform.Rotate(new Vector3(0, -3, 0));
+       if(Input.GetKey(backward)&&transform.eulerAngles.x<359)
+
+            transform.Rotate(new Vector3(0, 3, 0));
+        print(transform.eulerAngles.x);
+
+
+
+
+    }
+
+
+ 
+}

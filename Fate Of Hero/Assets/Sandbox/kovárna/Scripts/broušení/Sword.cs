2163da8d5785c14a0cae8a588cfe1f7ea50a46d2
vitekd55 <vicadvorak@seznam.cz>
2018-05-23 11:42:50 +0200

    omezeni pohybu

--- Fate Of Hero/Assets/Sandbox/kovárna/Scripts/broušení/Sword.cs
+++ Fate Of Hero/Assets/Sandbox/kovárna/Scripts/broušení/Sword.cs
@@ -8,31 +8,47 @@ public class Sword : MonoBehaviour {
     [SerializeField]
     private float moveSpeed,rotateSpeed;
     private GameObject sword;
+    private int turns = 0;
+    private float MaxPositionX,MinPositionX;
     private void Start()
     {
         sword = GameObject.Find("Sword");
-       
+        MaxPositionX = -9.433747f;
+
+        MinPositionX = -9.73f;
     }
     void Update ()
     {
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)&&transform.position.z<=MaxPositionX) 
             transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
 
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D)&&transform.position.z>=MinPositionX)
             transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
 
 
 
-        if (Input.GetKeyDown(KeyCode.S))
-            sword.transform.Rotate(new Vector3(0,0, rotateSpeed));
+        if (Input.GetKeyDown(KeyCode.S)&& turns>-1)
+        {
+            sword.transform.Rotate(new Vector3(0, 0, rotateSpeed));
+
+            turns--;
+
+        }
 
 
 
-        if (Input.GetKeyDown(KeyCode.W))
+
+
+        if (Input.GetKeyDown(KeyCode.W)&&turns < 1)
+        {
             sword.transform.Rotate(new Vector3(0, 0, -rotateSpeed));
+            turns++;
+        }
+
        
+      
 
     }
 

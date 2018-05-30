52a8b28756e4eaa08ca07b1be911b9891dd2c7a5
kedarcaja <kedarcaja@gmail.com>
2018-05-30 21:33:20 +0200

    3D Move

---
+++ Fate Of Hero/Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject gameObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(gameObject.transform.position.x + 0.44f, gameObject.transform.position.y+5f, gameObject.transform.position.z);
+	}
+}

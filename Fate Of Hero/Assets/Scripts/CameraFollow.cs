cfc9b00e8fd8dbe564072b4335914ea37d7c0c6f
kedarcaja <kedarcaja@gmail.com>
2018-06-01 22:01:06 +0200

    Uprava pohybáče

---
+++ Fate Of Hero/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    [SerializeField]
+    private GameObject gameObject;
+	
+	void Update () {
+
+        transform.position = new Vector3(gameObject.transform.position.x + 0.44f, gameObject.transform.position.y+12.88f, gameObject.transform.position.z-16.74f);
+	}
+}

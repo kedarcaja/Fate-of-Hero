8d7ba70c31b024480f2bfaf7c0f4e6a81f18f311
vitekd55 <vicadvorak@seznam.cz>
2018-06-09 21:50:40 +0200

    pozice, lock y

--- Fate Of Hero/Assets/Sandbox/kovárna/Scripts/Manager.cs
+++ Fate Of Hero/Assets/Sandbox/kovárna/Scripts/Manager.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-
-public class Manager : MonoBehaviour {
+using System.Timers;
+public class Manager : MonoBehaviour
+{
     [SerializeField]
-    private GameObject[] templates,parts;
+    private GameObject[] templates, parts;
     private Color clr = Color.white;
     private int index = 0;
     [SerializeField]
     private float speed;
     [SerializeField]
     private Camera myCamera;
-   private bool isDropped = false;
-	
-	void Start () {
+    private bool isDropped = false;
+
+    void Start()
+    {
         clr.a = 0;
         for (int i = 0; i < templates.Length; i++)
         {
@@ -24,13 +26,13 @@ public class Manager : MonoBehaviour {
 
         }
         parts[index].SetActive(true);
-
     }
 
 
-    void Update () {
+    void Update()
+    {
         GetInput();
-      
+        print(parts[index].transform.position.x);
     }
 
 
@@ -50,7 +52,11 @@ public class Manager : MonoBehaviour {
             {
                 if (hit.collider.tag == "part")
                 {
-                    parts[index].transform.position = new Vector3(myCamera.ScreenToWorldPoint(Input.mousePosition).x, parts[index].transform.position.y);
+                    if (myCamera.ScreenToWorldPoint(Input.mousePosition).y > -22)
+                        parts[index].transform.position = new Vector3(myCamera.ScreenToWorldPoint(Input.mousePosition).x, myCamera.ScreenToWorldPoint(Input.mousePosition).y);
+                    else
+                        parts[index].transform.position = new Vector3(myCamera.ScreenToWorldPoint(Input.mousePosition).x, parts[index].transform.position.y);
+
                 }
             }
         }
@@ -58,24 +64,30 @@ public class Manager : MonoBehaviour {
         {
             isDropped = true;
         }
-       
-        
-            
+
+
+
 
 
         if (isDropped)
         {
-            parts[index].transform.position = Vector3.Lerp(parts[index].transform.position, templates[index].transform.position,Time.deltaTime*10f);
-          
+            if (parts[index].transform.position.x < -1 && parts[index].transform.position.x > -3)
+                parts[index].transform.position = Vector3.Lerp(parts[index].transform.position, new Vector3(templates[index].transform.position.x, templates[index].transform.position.y), Time.deltaTime * 10f);
+            else if (parts[index].transform.position.x < -3)
+                parts[index].transform.position = Vector3.Lerp(parts[index].transform.position, new Vector3(templates[index].transform.position.x - 3, templates[index].transform.position.y), Time.deltaTime * 10f);
+            else if (parts[index].transform.position.x > -1)
+                parts[index].transform.position = Vector3.Lerp(parts[index].transform.position, new Vector3(templates[index].transform.position.x + 3, templates[index].transform.position.y), Time.deltaTime * 10f);
+
         }
-        if (templates[index].transform.position == parts[index].transform.position)
+        if (parts[index].transform.position == new Vector3(templates[index].transform.position.x, templates[index].transform.position.y) || parts[index].transform.position == new Vector3(templates[index].transform.position.x + 3, templates[index].transform.position.y )|| parts[index].transform.position == new Vector3(templates[index].transform.position.x-3, templates[index].transform.position.y))
         {
-            if(index<templates.Length-1)
-            index++;
+            if (index < templates.Length - 1)
+                index++;
             isDropped = false;
             parts[index].SetActive(true);
         }
     }
+}
  
   
-}
+

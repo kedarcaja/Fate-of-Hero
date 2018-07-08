9a302e3a51678be8c4bc4a26d1d137e2aff64fcc
kedarcaja <kedarcaja@gmail.com>
2018-07-08 14:50:31 +0200

    Pochodeň

---
+++ Fate Of Hero/Assets/3rdParty/Flames Of The Phoenix/GlowScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowScript : MonoBehaviour {
+
+	Renderer rend;
+	float colInt;
+	Color c;
+	public float minColInt = 0.5f, maxColInt = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<SpriteRenderer> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		colInt = Random.Range (minColInt, maxColInt);
+		c = rend.material.color;
+		c.a = colInt;
+		rend.material.color = c;
+	}
+}

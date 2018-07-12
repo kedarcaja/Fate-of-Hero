7233573168c2a88a4faf9b74c08b306afb505f2a
vitekd55 <vicadvorak@seznam.cz>
2018-07-13 01:04:11 +0200

    dialogy
    uvodni monolog + dialog s květinou-> nenačasovaný

---
+++ Fate Of Hero/Assets/Sandbox/Dvořák/JezirkoTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JezirkoTrigger : MonoBehaviour {
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag== "Player")
+        {
+            GetComponent<Subtitles>().Dialogs[0].trigger = true;
+        }
+    }
+}
---
+++ Fate Of Hero/Assets/Sandbox/Dvořák/Subtitles.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEditor.IMGUI;
+
+public class Subtitles : MonoBehaviour
+{
+    #region Class Variables
+    private static AudioSource audioSource;
+
+    [HideInInspector]
+    public static AudioSource audi
+    {
+        get
+        {
+            return audioSource;
+
+        }
+
+    }
+    private static Text txt;
+    [HideInInspector]
+    public static Text Txt
+    {
+        get { return txt; }
+    }
+    public Dialog[] Dialogs;
+    [Space]
+    [Space]
+    [Space]
+    [Space]
+    [Space]
+    [Space]
+    public Monolog[] Monologs;
+
+
+
+    #endregion
+
+    private void Start()
+    {
+        txt = FindObjectOfType<Text>();
+        audioSource = FindObjectOfType<Text>().GetComponent<AudioSource>();
+    }
+
+
+
+    private void Update()
+    {
+        #region Decisions Methods
+       
+        #endregion
+        #region Dialog Methods
+        for (int i = 0; i < Dialogs.Length; i++)
+        {
+            if (Dialogs[i].trigger)
+            {
+                Dialogs[i].StartDialog();
+                Dialogs[i].trigger = false;
+                StartCoroutine(Dialogs[i].StartTimer());
+
+            }
+            #endregion
+        }
+        #region Monolog Methods
+        for (int i = 0; i < Monologs.Length; i++)
+        {
+            Monologs[i].StartMonolog();
+            if (Monologs[i].trigger)
+            {
+                Monologs[i].trigger = false;
+            }
+
+        }
+
+        if (!audi.isPlaying)
+        {
+            audi.clip = null;
+            txt.text = "Empty";
+        }
+    }
+    #endregion
+  
+}
+
+
+
+
+
+#region Dialog
+
+[Serializable]
+        public struct Dialog
+        {
+
+          
+           
+            public string dialogName;
+            public AudioClip clip;
+            public bool trigger;
+    [Space]
+    [Space]
+    [SerializeField]
+    private DialogSentences[] sentences;
+    private DialogSentences currentSentence;
+   
+            private int sentenceIndex;
+
+    public void StartDialog()
+    {
+        if (trigger&&!Subtitles.audi.isPlaying)
+        {
+            sentenceIndex = 0;
+
+            currentSentence = sentences[sentenceIndex];
+
+         
+            Subtitles.audi.clip = clip;
+            Subtitles.audi.Play();
+            Subtitles.Txt.text = "<b>" + currentSentence.speaker + ": " + "</b>" + currentSentence.sentence;
+            currentSentence = sentences[sentenceIndex];
+
+        }
+
+
+      
+
+
+       
+    }
+    public IEnumerator StartTimer()
+    {
+        while (Subtitles.audi.isPlaying && Subtitles.audi.clip == clip)
+        {
+            yield return new WaitForSeconds(1);
+            currentSentence.timer--;
+            Debug.Log(currentSentence.timer);
+            if (currentSentence.timer <= 0 && sentenceIndex < sentences.Length - 1)
+            {
+                sentenceIndex++;
+                currentSentence = sentences[sentenceIndex];
+                Subtitles.Txt.text = "<b>" + currentSentence.speaker + ": " + "</b>" + currentSentence.sentence;
+
+
+            }
+        }
+    }
+
+  
+
+
+
+
+
+}
+[Serializable]
+public struct DialogSentences
+{
+
+    [Multiline]
+    public string sentence;
+
+    public string speaker;
+
+    [Range(1, 100)]
+
+    public float timer;
+
+}
+#endregion
+#region Monolog
+
+[Serializable]
+        public struct Monolog
+        {
+            #region Monolog Variables
+        
+            public string monologName;
+            public bool trigger;
+    [SerializeField]
+    private string speaker;
+
+            [Space]
+            [Space]
+
+
+            [Multiline]
+            [SerializeField]
+            private string sentences;
+            [SerializeField]
+            private AudioClip clip;
+            #endregion
+            public void StartMonolog()
+            {
+                if (trigger && !Subtitles.audi.isPlaying)
+                {
+                    Subtitles.Txt.text ="<b>"+speaker+":"+"</b>"+" "+ sentences;
+                    Subtitles.audi.clip = clip;
+                    Subtitles.audi.Play();
+                    trigger = false;
+
+
+                }
+
+            }
+
+        }
+
+#endregion
+
+
+

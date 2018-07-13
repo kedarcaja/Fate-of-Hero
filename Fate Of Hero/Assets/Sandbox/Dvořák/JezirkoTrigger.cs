c34b650b1ccd50da82d418f1765ce0b311b7372d
vitekd55 <vicadvorak@seznam.cz>
2018-07-13 15:42:35 +0200

    zastavení hráče, oprava blikání

--- Fate Of Hero/Assets/Sandbox/Dvořák/JezirkoTrigger.cs
+++ Fate Of Hero/Assets/Sandbox/Dvořák/JezirkoTrigger.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JezirkoTrigger : MonoBehaviour {
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.tag== "Player")
+        if(other.tag== "Player"&&!FindObjectOfType<Text>().GetComponent<AudioSource>().isPlaying&&Input.GetKeyDown(KeyCode.E))
         {
             GetComponent<Subtitles>().Dialogs[0].trigger = true;
         }

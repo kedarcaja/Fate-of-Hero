14f229cf61b30fd87f8670292cfe7081845ae3ca
kedarcaja <kedarcaja@gmail.com>
2018-07-14 18:07:27 +0200

    Monologi

---
+++ Fate Of Hero/Assets/Sandbox/Dvořák/DialogTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private bool monolog;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!monolog)
+        {
+            if (other.tag == "Player" && !FindObjectOfType<Text>().GetComponent<AudioSource>().isPlaying && Input.GetKeyDown(KeyCode.E))
+            {
+                GetComponent<Subtitles>().Dialogs[0].trigger = true;
+            }
+        }
+        else if (monolog)
+        {
+            if (other.tag == "Player" && !FindObjectOfType<Text>().GetComponent<AudioSource>().isPlaying)
+            {
+                
+                GetComponent<Subtitles>().Monologs[0].trigger = true;
+            }
+        }
+    }
+}

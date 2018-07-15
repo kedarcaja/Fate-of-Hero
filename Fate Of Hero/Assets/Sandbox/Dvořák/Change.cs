d1617d100e333b81ee7bea260a8fd7d2623f4e13
kedarcaja <kedarcaja@gmail.com>
2018-07-15 21:18:42 +0200

    Trigger

---
+++ Fate Of Hero/Assets/Sandbox/Dvořák/Change.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change : MonoBehaviour {
+
+	private GameObject first, second;
+
+    private void OnTriggerStay(Collider other)
+    {
+       
+        if (other.tag == "Player")
+        {
+            first.SetActive(false);
+            second.SetActive(true);
+        }
+    }
+    }

a22f5f83c006dc2d46d46e31cee33e17e2f66f3e
vitekd55 <vicadvorak@seznam.cz>
2018-07-26 19:14:07 +0200

    gven follow

---
+++ Fate Of Hero/Assets/Sandbox/Dvořák/Gven.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Gven : MonoBehaviour {
+	private NavMeshAgent agent;
+	[SerializeField]
+	private Transform portStart,portEnd,bridge,player,currentTarget;
+	private int currentIndex;
+	private bool startPort,agentFollow;
+	
+	
+	
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+
+		startPort = false;
+		agentFollow = true;
+		currentTarget = bridge;
+	}
+	
+	void Update () {
+
+		if (agentFollow)
+		{
+			agent.SetDestination(currentTarget.position);
+		}
+		if (startPort)
+		{
+			transform.position = portEnd.position;
+		}
+	
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.transform == bridge)
+		{
+			currentTarget = portStart;
+		}
+		if (other.transform == portStart)
+		{
+			agent.enabled = false;
+			startPort = true;
+		}
+		if (other.transform == portEnd)
+		{
+			startPort = false;
+			currentTarget = player;
+			agent.enabled = true;
+			agentFollow = true;
+		}
+	}
+	
+
+}

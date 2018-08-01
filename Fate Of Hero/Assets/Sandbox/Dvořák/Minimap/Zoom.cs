791d82e3c2c5be16827bca5eff5bd3a37e6cbf67
vitekd55 <vicadvorak@seznam.cz>
2018-08-02 00:01:59 +0200

    minimap zaklad

---
+++ Fate Of Hero/Assets/Sandbox/Dvořák/Minimap/Zoom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour {
+
+	private Camera cam;
+	[SerializeField]
+	private float maxZoom,minZoom;
+	void Start () {
+		cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void CameraZoom()
+	{
+		if(cam.orthographicSize<maxZoom)
+		cam.orthographicSize += 2;
+	}
+	public void CameraUnZoom()
+	{
+		if(cam.orthographicSize>minZoom)
+		cam.orthographicSize -= 2;
+	}
+}

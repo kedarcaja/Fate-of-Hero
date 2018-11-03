873efb78011cd392a6909b4d55de60e3055e20bd
kedarcaja <kedarcaja@gmail.com>
2018-11-03 11:10:31 +0100

    DoorAnimation
    -oprava NavMesh pro Dveře
    -oprava Animace Dveří
    
    -přidání Kolize zdí

---
+++ Fate Of Hero/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    private Animator anim;
+
+    
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            anim.SetTrigger("Open");
+           
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            anim.SetTrigger("Close");
+           
+        }
+    }
+}

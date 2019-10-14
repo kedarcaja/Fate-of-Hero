61a36ecce24a126d80da07274fb9addca019ddba
kedarcaja <kedarcaja@gmail.com>
2019-10-14 14:28:48 +0200

    oprava NavMesh

---
+++ Fate Of Hero/Assets/gameManagerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManagerScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] objects;
+
+    private void Awake()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            DontDestroyOnLoad(objects[i]);
+        }
+
+      
+    }
+}

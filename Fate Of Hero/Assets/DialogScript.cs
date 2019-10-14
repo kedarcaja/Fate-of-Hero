5ee30acb3be472f54e11fa5ad505ede0f9cd147f
kedarcaja <kedarcaja@gmail.com>
2019-10-14 23:06:44 +0200

    dialog

--- Fate Of Hero/Assets/DialogScript.cs
+++ Fate Of Hero/Assets/DialogScript.cs
@@ -8,16 +8,29 @@ public class DialogScript : MonoBehaviour
     [SerializeField]
     private DialogGraph graph;
 
+    [SerializeField]
+    private bool playOnTrigger = true, playOnAwake = false;
+
+   
+
     public DialogGraph Graph => graph;
 
     private void OnTriggerEnter(Collider other)
     {
-        DialogManager.Instance.ChangeGraph(this);
-        DialogManager.Instance.Play();
+        if (playOnTrigger)
+        {
+            DialogManager.Instance.ChangeGraph(this);
+            DialogManager.Instance.Play();
+        }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
 
+    private void Start()
+    {
+        if (playOnAwake)
+        {
+            DialogManager.Instance.ChangeGraph(this);
+            DialogManager.Instance.Play();
+        }
     }
 }
